2f4ab271a5d24dfb2abe80802561d0cd47caebb5
Phahladira-Moja <phahladira@patala.co.za>
2023-10-14 20:44:03 +0200

    Added the typeorm

---
+++ src/typeorm/User.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({
+    type: 'bigint',
+  })
+  id: number;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
+  username: string;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
+  email: string;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
+  password: string;
+}
