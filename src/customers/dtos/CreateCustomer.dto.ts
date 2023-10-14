ac089f2ad59e54bb235e7ea5163a8d9723945142
Phahladira-Moja <phahladira@patala.co.za>
2023-10-14 18:04:09 +0200

    Added validation to endpoints

--- src/customers/dtos/CreateCustomer.dto.ts
+++ src/customers/dtos/CreateCustomer.dto.ts
@@ -1,7 +1,28 @@
 /* eslint-disable prettier/prettier */
 
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumberString,
+  ValidateNested,
+  IsNotEmptyObject,
+} from 'class-validator';
+import { CreateAddressDto } from './CreateAddressDto';
+import { Type } from 'class-transformer';
+
 export class CreateCustomerDto {
+  @IsEmail()
   email: string;
+
+  @IsNumberString()
+  @IsNotEmpty()
   id: number;
+
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmptyObject()
+  @ValidateNested()
+  @Type(() => CreateAddressDto)
+  address: CreateAddressDto;
 }
